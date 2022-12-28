e766981531248da2b3a80a22285583b5c091c8c6
Lampirou <win.moi@hotmail.com>
2022-12-28 21:29:44 +0100

    Add control for light bulb

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'aumy-front';
+
+  public x: number = -1;
+  public y: number = -1;
+  public height: number = 0;
+  public heightInPercent: number = 0;
+  public isClicked: boolean = false;
+
+  @ViewChild('myDiv') myDiv: ElementRef<HTMLDivElement> = {} as any;
+  @ViewChild('progressValue') progressValue: ElementRef<HTMLDivElement> = {} as any;
+
+  mouseMoved($event: MouseEvent) {
+    if(!this.isClicked) return;
+
+    this.x = $event.clientX - this.myDiv.nativeElement.offsetLeft;
+    this.y = $event.clientY - this.myDiv.nativeElement.offsetTop;
+
+    let newHeight = this.myDiv.nativeElement.clientHeight - this.y;
+    this.heightInPercent = newHeight / this.myDiv.nativeElement.clientHeight * 100;
+
+    if(this.heightInPercent < 0 || this.heightInPercent > 100) return;
+    this.height = newHeight;
+  }
+
+  mouseDown($event: MouseEvent) {
+    this.isClicked = true;
+
+    this.mouseMoved($event);
+  }
 }
